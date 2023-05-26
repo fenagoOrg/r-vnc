c1076ce8fde90217da9a52c86474a02776502d1d
Dr. Ernesto Lee <socrates73@gmail.com>
2023-05-26 07:04:26 -0700

    Update install_r.sh

--- install_r.sh
+++ install_r.sh
@@ -11,3 +11,5 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 sudo apt install --no-install-recommends r-base
 sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2023.03.1-446-amd64.deb
+sudo dpkg -i ./rstudio-2023.03.1-446-amd64.deb
